9ff267a8ad8c01ebcfd98f5bf08d6e2285cf62cf
Mike De Decker <41482163+Mike-MultiSafepay@users.noreply.github.com>
2019-11-18 13:20:51 +0100

    PLGSHPS6-34: Add EPS

--- src/Helper/GatewayHelper.php
+++ src/Helper/GatewayHelper.php
@@ -10,6 +10,7 @@ use MultiSafepay\Shopware6\PaymentMethods\AfterPay;
 use MultiSafepay\Shopware6\PaymentMethods\AmericanExpress;
 use MultiSafepay\Shopware6\PaymentMethods\Bancontact;
 use MultiSafepay\Shopware6\PaymentMethods\DirectDebit;
+use MultiSafepay\Shopware6\PaymentMethods\Eps;
 use MultiSafepay\Shopware6\PaymentMethods\Giropay;
 use MultiSafepay\Shopware6\PaymentMethods\Ideal;
 use MultiSafepay\Shopware6\PaymentMethods\Mastercard;
@@ -24,6 +25,7 @@ class GatewayHelper
         AmericanExpress::class,
         Bancontact::class,
         DirectDebit::class,
+        Eps::class,
         Giropay::class,
         Ideal::class,
         Mastercard::class,
