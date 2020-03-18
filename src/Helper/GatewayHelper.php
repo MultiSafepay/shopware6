5286b0e3f9e0a724aa5e4f680df0932861601801
Ronald Schonewille <Ronald.Schonewille@MultiSafepay.com>
2020-03-18 10:32:50 +0100

    PLGSHPS6-134: Add Direct Bank Transfer

--- src/Helper/GatewayHelper.php
+++ src/Helper/GatewayHelper.php
@@ -16,6 +16,7 @@ use MultiSafepay\Shopware6\PaymentMethods\Belfius;
 use MultiSafepay\Shopware6\PaymentMethods\Betaalplan;
 use MultiSafepay\Shopware6\PaymentMethods\Boekenbon;
 use MultiSafepay\Shopware6\PaymentMethods\DirectDebit;
+use MultiSafepay\Shopware6\PaymentMethods\DirectBankTransfer;
 use MultiSafepay\Shopware6\PaymentMethods\Dotpay;
 use MultiSafepay\Shopware6\PaymentMethods\Einvoice;
 use MultiSafepay\Shopware6\PaymentMethods\Eps;
@@ -66,6 +67,7 @@ class GatewayHelper
         Betaalplan::class,
         Boekenbon::class,
         DirectDebit::class,
+        DirectBankTransfer::class,
         Dotpay::class,
         Einvoice::class,
         Eps::class,
