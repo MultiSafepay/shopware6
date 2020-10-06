0ca1b2713988a3a55d1da379110440ea92aab877
Mike De Decker <41482163+Mike-MultiSafepay@users.noreply.github.com>
2020-10-06 18:49:02 +0200

    [feature] Add support tab

---
+++ src/Resources/app/administration/src/components/multisafepay-support/index.js
@@ -0,0 +1,8 @@
+const {Component} = Shopware;
+import './multisafepay-support.scss'
+import template from './multisafepay-support.html.twig';
+
+
+Component.register('multisafepay-support', {
+    template,
+});
