0ae95d230a90c8cb4d64545265602fc194816d38
Joshua Behrens <behrens@heptacom.de>
2021-04-13 02:09:50 +0200

    Add missing semicolons

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,9 +1,9 @@
 import { Component, Application } from 'src/core/shopware';
-import './components/multisafepay-refund'
-import './components/multisafepay-verify-api-key'
-import './components/multisafepay-support'
-import template from './extension/sw-order-detail/sw-order-detail.html.twig'
-import MultiSafepayApiService from './service/MultiSafepayApi.service'
+import './components/multisafepay-refund';
+import './components/multisafepay-verify-api-key';
+import './components/multisafepay-support';
+import template from './extension/sw-order-detail/sw-order-detail.html.twig';
+import MultiSafepayApiService from './service/MultiSafepayApi.service';
 
 
 Component.override('sw-order-detail-base', {
