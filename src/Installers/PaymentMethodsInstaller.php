4d8d05cc668f84fa8d711ef7fc4e7f382bd9f872
Mike De Decker <41482163+Mike-MultiSafepay@users.noreply.github.com>
2023-06-30 09:43:58 +0200

    PLGSHPS6-326: Don't overwrite payment method data on update

--- src/Installers/PaymentMethodsInstaller.php
+++ src/Installers/PaymentMethodsInstaller.php
@@ -6,9 +6,6 @@
 
 namespace MultiSafepay\Shopware6\Installers;
 
-use MultiSafepay\Shopware6\Handlers\GenericPaymentHandler;
-use MultiSafepay\Shopware6\Handlers\GenericPaymentHandler2;
-use MultiSafepay\Shopware6\Handlers\GenericPaymentHandler3;
 use MultiSafepay\Shopware6\MltisafeMultiSafepay;
 use MultiSafepay\Shopware6\PaymentMethods\IngHomePay;
 use MultiSafepay\Shopware6\PaymentMethods\MultiSafepay;
@@ -120,12 +117,7 @@ class PaymentMethodsInstaller implements InstallerInterface
 
         $mediaId = $this->getMediaId($paymentMethod, $context);
 
-        if ($paymentMethodId !== null
-            && in_array($paymentMethod->getPaymentHandler(), [
-                GenericPaymentHandler::class,
-                GenericPaymentHandler2::class,
-                GenericPaymentHandler3::class,
-            ])) {
+        if ($paymentMethodId) {
             return;
         }
 
